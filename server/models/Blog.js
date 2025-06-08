3a6340cad70de67beec7ef2445ca020acda9f4d7
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-06-08 17:17:51 +0530

    Added backend validation to check for bad words before saving blog

--- server/models/Blog.js
+++ server/models/Blog.js
@@ -3,9 +3,15 @@ import mongoose from 'mongoose';
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  isAnonymous: { type: Boolean, default: true },
-  createdAt: { type: Date, default: Date.now }
+  tags: [String],
+  authorId: {
+    type: String,
+    default: 'Anonymous'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-const Blog = mongoose.model('Blog', blogSchema);
-export default Blog;
+export default mongoose.model('Blog', blogSchema);
