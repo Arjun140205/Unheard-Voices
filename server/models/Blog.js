a2c24806966daa6b62b03f50dd3ff4355c652ffb
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-06-11 04:16:39 +0530

    Added reactions field to Blog model schema

--- server/models/Blog.js
+++ server/models/Blog.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import slugify from 'slugify';
+import { v4 as uuidv4 } from 'uuid';
 
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
@@ -11,7 +13,27 @@ const blogSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now
+  },
+  slug: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  reactions: {
+    related: { type: Number, default: 0 },
+    thoughtful: { type: Number, default: 0 },
+    touched: { type: Number, default: 0 },
+    inspired: { type: Number, default: 0 }
   }
+});
+
+// Generate unique slug before saving
+blogSchema.pre('validate', function (next) {
+  if (this.title && !this.slug) {
+    const uniqueId = uuidv4().slice(0, 6);
+    this.slug = slugify(this.title, { lower: true, strict: true }) + '-' + uniqueId;
+  }
+  next();
 });
 
 export default mongoose.model('Blog', blogSchema);
