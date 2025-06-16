87fd5269335e4c95d03b46173453d54fd1c76513
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-06-16 20:53:01 +0530

    Extended Blog model to support flagging and polling metadata

--- server/models/Blog.js
+++ server/models/Blog.js
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
+  flagged: { type: Boolean, default: false },
   tags: [String],
   authorId: {
     type: String,
@@ -29,7 +30,11 @@ const blogSchema = new mongoose.Schema({
     thoughtful: { type: Number, default: 0 },
     touched: { type: Number, default: 0 },
     inspired: { type: Number, default: 0 }
-  }
+  },
+  poll: {
+  yes: { type: Number, default: 0 },
+  no: { type: Number, default: 0 }
+}
 });
 
 // Generate unique slug before saving
@@ -40,5 +45,7 @@ blogSchema.pre('validate', function (next) {
   }
   next();
 });
+
+
 
 export default mongoose.model('Blog', blogSchema);
