05cc0ff8c15f33d0cf0588459a9d3d3ce204728a
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-06-19 23:39:37 +0530

    updated: added loading indicator support for blog content rendering

--- client/src/pages/BlogDetails.jsx
+++ client/src/pages/BlogDetails.jsx
@@ -5,6 +5,7 @@ import { useInView } from 'react-intersection-observer';
 import { recentBlogsCache } from "../utils/cache";
 import LazyImage from "../components/LazyImage";
 import Error from "../components/Error";
+import Loader from "../components/Loader";
 
 
 export default function BlogDetails() {
@@ -132,11 +133,7 @@ export default function BlogDetails() {
       
       <div className="max-w-4xl mx-auto px-4 py-8">
         {loading ? (
-          <div className="animate-pulse space-y-4">
-            <div className="h-8 bg-gray-200 rounded w-3/4"></div>
-            <div className="h-4 bg-gray-200 rounded w-1/2"></div>
-            <div className="h-64 bg-gray-200 rounded"></div>
-          </div>
+          <Loader />
         ) : blog ? (
           <>
             <h1 className="text-4xl font-bold mb-4">{blog.title}</h1>
